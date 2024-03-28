f6f719f6b674cb2941f0d4489222cbecd150e0de
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2024-03-28 14:12:42 -0700

    totals, R non-test fails

--- results-2024-03-27.R
+++ results-2024-03-27.R
@@ -19,14 +19,15 @@ with(data.list, lines[!mutant.results, on="file"])
 with(data.list, mutant.results[!lines, on="file"])
 data.list$mutant[, .(software, file, line)]
 data.list$lines
+only.controls <- data.list$mutant.results[, .(results=.N), by=.(software, file)][results==1]
+some.mutants <- data.list$mutant[!only.controls, on="file"]
 
-data.list$mutant[is.na(line), table(ExitCode)]
+some.mutants[is.na(line), table(ExitCode)]
 passing.codes <- c("NOTE:installed package size", "0:0")
-data.list$mutant[
+some.mutants[
 , passed := ExitCode %in% passing.codes & State_blank=="COMPLETED"
 ][]
-mutants <- data.list$mutant[!is.na(line)]
-mutant.counts <- mutants[, .(
+mutant.counts <- some.mutants[!is.na(line), .(
   n.mutants=.N,
   n.passing=sum(passed)
 ), by=.(software, file, line)
@@ -106,7 +107,7 @@ print(
 
 ##TODO time.
 library(ggplot2)
-data.list$mutant[, `:=`(
+some.mutants[, `:=`(
   result = factor(fcase(
     is.na(line), "control",
     passed, "pass",
@@ -120,59 +121,47 @@ data.list$mutant[, `:=`(
 ][
 , years := days/365.25
 ][]
-mutant.stats <- dcast(
-  data.list$mutant,
-  result + software ~ .,
-  list(length, median, sum),
-  value.var=c("hours","minutes","days","weeks","years")
-)[, time_sum := NA_character_][]
-for(unit.name in c("year","week","day","hour")){
-  units <- paste0(unit.name,"s")
-  unit.col <- paste0(units,"_sum")
-  sum.vec <- mutant.stats[[unit.col]]
-  mutant.stats[
-    sum.vec>1 & is.na(time_sum),
-    time_sum := sprintf("%.1f %s", get(unit.col), units)
-  ][]
+time_sum <- function(form){
+  form.stats <- dcast(
+    some.mutants,
+    form,
+    list(length, median, sum),
+    value.var=c("hours","minutes","days","weeks","years")
+  )[, time_sum := NA_character_][]
+  for(unit.name in c("year","week","day","hour")){
+    units <- paste0(unit.name,"s")
+    unit.col <- paste0(units,"_sum")
+    sum.vec <- form.stats[[unit.col]]
+    form.stats[
+      sum.vec>1 & is.na(time_sum),
+      time_sum := sprintf("%.1f %s", get(unit.col), units)
+    ][]
+  }
+  form.stats
 }
+software.stats <- time_sum(software ~ .)[, .(
+  software,
+  time_total=time_sum,
+  mutants=paste(days_length, "mutants")
+)][
+, soft.time.mutants := paste0(software, ", ", mutants, ", ", time_total)
+][]
 med.color="red"
-ggplot()+
-  geom_histogram(aes(
-    hours),
-    bins=100,
-    data=data.list$mutant)+
-  geom_vline(aes(
-    xintercept=hours_median),
-    color=med.color,
-    data=mutant.stats)+
-  geom_label(aes(
-    hours_median, Inf,
-    label=sprintf("median=%.1f minutes", hours_median*60)),
-    color=med.color,
-    alpha=0.5,
-    hjust=0,
-    vjust=1,
-    data=mutant.stats)+
-  geom_text(aes(
-    0, Inf,
-    label=sprintf("%s mutants\n%s",hours_length,time_sum)),
-    hjust=0,
-    vjust=1,
-    data=mutant.stats)+
-  facet_grid(result ~ software, scales="free")+
-  scale_x_log10(
-    breaks=c(0.001, 0.002, 0.004, 0.01, 0.02, 0.04, 0.1, 0.2, 0.4, 1, 2))
-
 text.size <- 3
+addMeta <- function(DT){
+  DT[software.stats, on=.(software)]
+}
+hist.dt <- addMeta(some.mutants)
+result.stats <- addMeta(time_sum(result + software ~ .))
 gg <- ggplot()+
   geom_histogram(aes(
     minutes),
     bins=100,
-    data=data.list$mutant)+
+    data=hist.dt)+
   geom_vline(aes(
     xintercept=minutes_median),
     color=med.color,
-    data=mutant.stats)+
+    data=result.stats)+
   geom_label(aes(
     minutes_median, Inf,
     label=sprintf("median=%.1f minutes", minutes_median)),
@@ -181,15 +170,15 @@ gg <- ggplot()+
     hjust=0,
     size=text.size,
     vjust=1,
-    data=mutant.stats)+
+    data=result.stats)+
   geom_text(aes(
     ifelse(software=="pandas" & result=="error", 50, 0), Inf,
     label=sprintf("%s mutants\n%s",hours_length,time_sum)),
     size=text.size,
     hjust=0,
     vjust=1,
-    data=mutant.stats)+
-  facet_grid(result ~ software, scales="free")+
+    data=result.stats)+
+  facet_grid(result ~ soft.time.mutants, scales="free")+
   scale_x_log10(
     "Time to build and check (minutes)",
     breaks=c(0.1, 0.2, 0.4, 1, 2, 4, 10, 20, 40, 100, 200))+
@@ -198,3 +187,21 @@ gg <- ggplot()+
 png("results-2024-03-27-time-hist.png", width=7, height=3, units="in", res=300)
 print(gg)
 dev.off()
+##why is pandas bimodal?
+
+##how many data.table failures include test failures?
+dt.failures <- some.mutants[
+  software=="data.table" & result=="fail"
+][
+, tests.failed := grepl("ERROR:tests", ExitCode)
+][]
+dt.failures[, table(tests.failed)]
+(msg.counts <- dt.failures[tests.failed==FALSE, .(
+  msg=unlist(strsplit(ExitCode, split=", "))
+)][, .(count=.N), by=msg][order(-count)])
+(out.counts <- msg.counts[!msg %in% passing.codes])
+xt <- xtable(out.counts, digits=1)
+print(
+  xt,
+  type="latex", floating=FALSE, include.rownames=FALSE,
+  format.args = list(big.mark = ","))
